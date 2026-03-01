7056eedfc2f8d299ddbba871b2d996b67648425a
MUTEBI605 <smutebius@yahoo.com>
2026-03-01 08:58:18 +0300

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,15 @@
+// Placeholder - replace this later when you have Firebase set up
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyDummyKey1234567890",
+      authDomain: "famtree-12345.firebaseapp.com",
+      projectId: "famtree-12345",
+      storageBucket: "famtree-12345.appspot.com",
+      messagingSenderId: "123456789",
+      appId: "1:123456789:android:abcdef123456",
+    );
+  }
+}
